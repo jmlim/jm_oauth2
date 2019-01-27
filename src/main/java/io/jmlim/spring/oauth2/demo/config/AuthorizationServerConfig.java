9942b949ebd29163290e5765d7b1371e1020dc8b
Lim Jeong muk <hackerljm@gmail.com>
2019-01-27 23:04:29 +0900

    TokenStore Database 저장 방식에서 JWT 로 변경.

--- src/main/java/io/jmlim/spring/oauth2/demo/config/AuthorizationServerConfig.java
+++ src/main/java/io/jmlim/spring/oauth2/demo/config/AuthorizationServerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 /**
  *
@@ -23,6 +24,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     /**기본 inmemory (추후 database 기반으로 변경 필요) */
     @Autowired
     private TokenStore tokenStore;
+
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
 
     /** authenticationManager는 아래 configure의 withClient 와 secret 를 보고 실제 인증을 처리함.*/
     @Autowired
@@ -40,10 +44,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .inMemory()
                 .withClient("jmlim-client") // client Id
                 .secret(passwordEncoder.encode("jmlim-password")) // client password
+                // 엑세스 토큰 발급 가능한 인증타입.
                 .authorizedGrantTypes("password",
                         "authorization_code",
                         "refresh_token",
                         "implicit")
+                // 이 클라이언트로 접근할 수 있는 범위 제한.
+                // 해당 클라이언트로 API 접근했을 때 접근 범위를 제한시키는 속성.
                 .scopes("read", "write", "trust")
                 // access 토큰 만료 시간
                 .accessTokenValiditySeconds(1*60*60)
@@ -57,6 +64,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore)
+                .accessTokenConverter(accessTokenConverter)
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
     }
--- src/main/java/io/jmlim/spring/oauth2/demo/config/SecurityConfig.java
+++ src/main/java/io/jmlim/spring/oauth2/demo/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,14 +35,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-/*    @Bean
+    /*
+    // Inmemory
+    @Bean
     public TokenStore tokenStore() {
         return new InMemoryTokenStore();
     }*/
 
+    /*
+    // Database
     @Bean
     public TokenStore jdbcTokenStore(DataSource dataSource) {
         return new JdbcTokenStore(dataSource);
+    }*/
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        return jwtAccessTokenConverter;
     }
 
     @Bean
